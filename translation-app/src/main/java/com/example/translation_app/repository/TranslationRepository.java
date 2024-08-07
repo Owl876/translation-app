4e52b16630be950b9f2ef22f167f8d3bf7b854b4
Owl876 <dobretskovda@gmail.com>
2024-08-07 17:17:23 +0300

    updates

--- translation-app/src/main/java/com/example/translation_app/repository/TranslationRepository.java
+++
@@ -1,8 +0,0 @@
-package main.translationapi.repository;
-
-import main.translationapi.entity.TranslationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TranslationRepository extends JpaRepository<TranslationEntity, Long> {
-
-}
