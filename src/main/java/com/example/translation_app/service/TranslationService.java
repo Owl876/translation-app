f1f8e494a98824975e79cf2a5fa5594a9cacd5bf
Dmitriy <133746385+Owl876@users.noreply.github.com>
2024-08-07 17:27:12 +0300

    final

---
+++ src/main/java/com/example/translation_app/service/TranslationService.java
@@ -0,0 +1,114 @@
+package main.translationapi.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import main.translationapi.client.TranslationClient;
+import main.translationapi.dto.TranslationRequest;
+import main.translationapi.dto.TranslationResponse;
+import main.translationapi.dto.YandexApiRequest;
+import main.translationapi.dto.YandexApiResponse;
+import main.translationapi.entity.TranslationEntity;
+import main.translationapi.exception.AuthenticationException;
+import main.translationapi.exception.LanguageNotFoundException;
+import main.translationapi.exception.TooManyRequestsException;
+import main.translationapi.exception.TranslationResourceAccessException;
+import main.translationapi.mapper.YandexRequestMapper;
+import main.translationapi.mapper.YandexResponseMapper;
+import main.translationapi.repository.TranslationRepository;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Service
+public class TranslationService {
+
+    private final TranslationRepository translationRepository;
+    private final TranslationClient translationClient;
+    private final RestTemplate translator;
+    private final YandexRequestMapper yandexRequestMapper;
+    private final YandexResponseMapper yandexResponseMapper;
+    private final ExecutorService executorService;
+
+    public TranslationService(TranslationRepository translationRepository,
+                              RestTemplate translator,
+                              TranslationClient translationClient,
+                              YandexResponseMapper yandexResponseMapper,
+                              YandexRequestMapper yandexRequestMapper) {
+
+        this.translationRepository = translationRepository;
+        this.translator = translator;
+        this.translationClient = translationClient;
+        this.yandexRequestMapper = yandexRequestMapper;
+        this.yandexResponseMapper = yandexResponseMapper;
+        this.executorService = Executors.newFixedThreadPool(10);
+    }
+
+    public TranslationResponse translate(TranslationRequest translationRequest, HttpServletRequest request) {
+
+        if (translationRequest.getOriginalLang() == null || translationRequest.getOriginalLang().isEmpty()){
+            throw new LanguageNotFoundException();
+        } else if (translationRequest.getTranslatedLang() == null || translationRequest.getTranslatedLang().isEmpty()){
+            throw new LanguageNotFoundException("Не найден язык, на который необходимо перевести текст.");
+        }
+
+        String[] wordsToTranslate = translationRequest.getTextToTranslate().split("\\s+");
+        List<Future<TranslationResponse>> futures = new ArrayList<>();
+
+        for (String word : wordsToTranslate) {
+            futures.add(executorService.submit(() -> translateWord(new TranslationRequest(word,
+                    translationRequest.getOriginalLang(),
+                    translationRequest.getTranslatedLang()))));
+        }
+
+        List<String> translatedWords = new ArrayList<>();
+        for (Future<TranslationResponse> future : futures) {
+            try {
+                TranslationResponse response = future.get();
+                translatedWords.add(response.getTranslatedText());
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String translatedText = String.join(" ", translatedWords);
+        TranslationResponse translationResponse = new TranslationResponse(translatedText);
+
+        TranslationEntity translationEntity = new TranslationEntity();
+        translationEntity.setOriginalText(translationRequest.getTextToTranslate());
+        translationEntity.setTranslatedText(translatedText);
+        if (request != null) translationEntity.setIp(request.getRemoteAddr());
+        translationRepository.save(translationEntity);
+
+        return translationResponse;
+    }
+
+    private TranslationResponse translateWord(TranslationRequest translationRequest) {
+
+        YandexApiRequest yandexApiRequest = yandexRequestMapper.mapToYandexRequest(translationRequest);
+        TranslationResponse translationResponse = new TranslationResponse();
+
+        try {
+            YandexApiResponse yandexApiResponse = translator.postForObject(translationClient.getBaseUrl(), yandexApiRequest, YandexApiResponse.class);
+            translationResponse = yandexResponseMapper.mapToTranslationResponse(yandexApiResponse);
+        }catch (HttpClientErrorException e){
+            HttpStatusCode httpStatusCode = e.getStatusCode();
+            if (httpStatusCode == HttpStatusCode.valueOf(429)){
+                throw new TooManyRequestsException();
+            } else if (httpStatusCode == HttpStatusCode.valueOf(400)){
+                throw new TranslationResourceAccessException();
+            } else if (httpStatusCode == HttpStatusCode.valueOf(401)){
+                throw new AuthenticationException();
+            }
+        }
+        return translationResponse;
+    }
+}
